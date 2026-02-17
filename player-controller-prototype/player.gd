242843bb40ff8766eee9c71bf689840c42d014f9
Max Messier <max.bradley.messier@gmail.com>
2026-02-16 20:09:50 -0500

    added player movement system
    - added speed
    - _process func
    - added input direction and detection for player
    - applied movement w/ position +=

--- player-controller-prototype/player.gd
+++ player-controller-prototype/player.gd
@@ -1,1 +1,26 @@
 extends Node2D
+
+#Speed
+var speed : float = 200
+
+#Player Movement
+func _process(delta: float) -> void:
+	#Store Direction
+	var direction : Vector2 = Vector2.ZERO
+
+#Apply Movement
+	#Read Input
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+	
+	position += direction * speed * delta
+	pass
+	
+	
+	
