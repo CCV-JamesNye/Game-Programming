2186203c3d138e1adfc070dd903bcac3a7fd3453
Max Messier <max.bradley.messier@gmail.com>
2026-02-22 19:06:41 -0500

    Gravity & Velocity
    -added gravity and adjusted velocity

--- player-controller-prototype/Scripts/skater-character-body.gd
+++ player-controller-prototype/Scripts/skater-character-body.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 #Speed
 @export var speed : float = 200
+@export var gravity : float = 980.0
+
 
 #Node enters scene tree
 func _ready() -> void: 
@@ -11,20 +13,19 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 
-func _physics_process(_delta: float) -> void:
+func _physics_process( delta: float) -> void:
+	
 		#Store Direction
 	var direction : Vector2 = Vector2.ZERO
-
+	velocity.y += gravity * delta
+	
 #Apply Movement
 	#Read Input
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
-	if Input.is_action_pressed("move_down"):
-		direction.y += 1
-	if Input.is_action_pressed("move_up"):
-		direction.y -= 1
 	
-	velocity = direction.normalized() * speed
+	velocity.x = direction.normalized().x * speed
 	move_and_slide()
+	
