125ac05c87398a3db8b39a0b8374df0f3a5953b9
Max Messier <max.bradley.messier@gmail.com>
2026-03-25 15:41:04 -0400

    Finish Enemy Behavior
    finished enemy behavior
    -fixed layer and mask issue
    -changed upper platform to rectangle instead of world boundary which caused issues with the player
    -finished patrol script
    -added flip to enemy so he faced the direction he's walking

--- player-controller-prototype/Scripts/enemy_vita.gd
+++ player-controller-prototype/Scripts/enemy_vita.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-@export var patrol_speed : float = 50.0
+@export var patrol_speed : float = 200.0
 @export var gravity : float = 980
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var floor_detector: RayCast2D = $FloorDetector
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 var direction : Vector2 = Vector2.RIGHT
@@ -16,16 +17,6 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	move_and_slide()
-	if !floor_detector.is_colliding():
-		if direction == Vector2.RIGHT:
-			direction=Vector2.LEFT
-			floor_detector.position.x = -23
-		else:
-			direction=Vector2.RIGHT
-			floor_detector.position.x = 23
-			
-
 func _process(_delta: float) -> void:
 	if current_state == STATE.IDLE:
 		velocity=Vector2.ZERO
@@ -33,3 +24,14 @@ func _process(_delta: float) -> void:
 	elif current_state == STATE.PATROL:
 		animation_player.play("walk")
 		velocity.x=direction.x*patrol_speed
+		
+	move_and_slide()
+	if !floor_detector.is_colliding():
+		if direction == Vector2.RIGHT:
+			direction=Vector2.LEFT
+			floor_detector.position.x = -23
+			sprite_2d.flip_h = true
+		else:
+			direction=Vector2.RIGHT
+			floor_detector.position.x = 23
+			sprite_2d.flip_h = false
